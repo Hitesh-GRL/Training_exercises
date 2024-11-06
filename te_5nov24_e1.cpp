ebde528bf07e21a6cf3281f9eef3f9d3c957f554
Hitesh-GRL <hitesh.grl.id@gmail.com>
2024-11-06 03:17:48 -0800

    upload te_5nov24_e1

---
+++ te_5nov24_e1.cpp
@@ -0,0 +1,10 @@
+// Program to observe the exit status when main function returns any value other than 0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "HELLO";
+   return 10; 
+}
