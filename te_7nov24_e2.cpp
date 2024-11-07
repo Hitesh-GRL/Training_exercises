a29bfbfb65869357e59fa1f5dc4d06db282e5927
Hitesh-GRL <hitesh.grl.id@gmail.com>
2024-11-07 16:41:08 +0530

    new files added
    new exercises

---
+++ te_7nov24_e2.cpp
@@ -0,0 +1,12 @@
+// Program to demonstrate basic input and output operations 
+
+#include <iostream>
+
+int main()
+{   
+    int age;
+    std::cout << "Enter your age\n";
+    std::cin >> age;
+    std::cout << "Your age is " << age;
+    return 0;
+}
---
+++ te_7nov24_e3.cpp
@@ -0,0 +1,21 @@
+// Program to accept different details of a student
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int student_id = 035;
+int student_age = 21;
+float student_fee = 1.2;
+char student_grade = 'A';
+
+// Print variables
+cout << "Student ID: " << student_id << "\n";
+cout << "Student Age: " << student_age << "\n";
+cout << "Student Fee: " << student_fee << "\n";
+cout << "Student Grade: " << student_grade << "\n";
+
+return 0;
+}
